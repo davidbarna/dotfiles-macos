da252972e292e5eff2a4486207bdc0a65439aea8
David Almeida <1083195+davidbarna@users.noreply.github.com>
2021-07-11 19:32:50 +1000

    Make brew install conditional

--- setup-homebrew.zsh
+++ setup-homebrew.zsh
@@ -2,8 +2,13 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+# Install brew only if uninstalled
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Install all packages and apps
 brew bundle --verbose
