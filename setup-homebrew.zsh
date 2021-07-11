f15c74df7f623d9a7fbf59024d82079805735395
David Almeida <1083195+davidbarna@users.noreply.github.com>
2021-07-11 16:47:27 +1000

    Add homebrew setup + apps

---
+++ setup-homebrew.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Command line tools
+brew install httpie
+brew install bat
+
+# Apps
+brew install google-chrome
+brew install visual-studio-code
